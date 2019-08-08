b9d592cdcbd7d71c59f12e874574b3bbdd7442ff
Divij Gupta <divijgupta66@gmail.com>
2019-08-09 00:58:44 +0530

    Second

---
+++ app/src/main/java/divij/com/unihyr/UsersFragment.java
@@ -0,0 +1,32 @@
+package divij.com.unihyr;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class UsersFragment extends Fragment {
+
+
+    public UsersFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v=inflater.inflate(R.layout.fragment_users, container, false);
+        return v;
+    }
+
+}
