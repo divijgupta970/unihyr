252921411cae4dc7ff274e4b9f62b905e08eb80d
Divij Gupta <divijgupta66@gmail.com>
2019-09-20 20:55:07 +0530

    profiles spinners set

--- app/src/main/java/divij/com/unihyr/VPPActiveProfilesFragment.java
+++ app/src/main/java/divij/com/unihyr/VPPActiveProfilesFragment.java
@@ -8,13 +8,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class VPPActiveProfilesFragment extends Fragment {
-
+    Spinner selectChannelSpinner;
+    Spinner selectStatusSpinner;
 
     public VPPActiveProfilesFragment() {
         // Required empty public constructor
@@ -25,7 +28,18 @@ public class VPPActiveProfilesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_vppactive_profiles, container, false);
+        View v=inflater.inflate(R.layout.fragment_vppactive_profiles, container, false);
+        selectChannelSpinner=v.findViewById(R.id.spinnerSelectChannel);
+        selectStatusSpinner=v.findViewById(R.id.spinnerSelectStatus);
+        ArrayAdapter<CharSequence> adapter1= ArrayAdapter.createFromResource(getActivity(),
+                R.array.select_status_spinner_array, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(getActivity(),
+                R.array.select_chanel_spinner_array, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        selectChannelSpinner.setAdapter(adapter2);
+        selectStatusSpinner.setAdapter(adapter1);
+        return v;
     }
 
 }
