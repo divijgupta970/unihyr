2daaa20a0bb48f4e74264c2e2c8c1ca53696b089
Divij Gupta <divijgupta66@gmail.com>
2019-09-21 21:50:10 +0530

    added drives

--- app/src/main/java/divij/com/unihyr/ViewPositionsDrivesFragment.java
+++ app/src/main/java/divij/com/unihyr/ViewPositionsDrivesFragment.java
@@ -8,13 +8,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ViewPositionsDrivesFragment extends Fragment {
-
+    Spinner spinner;
 
     public ViewPositionsDrivesFragment() {
         // Required empty public constructor
@@ -25,7 +27,13 @@ public class ViewPositionsDrivesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_positions_drives, container, false);
+        View v=inflater.inflate(R.layout.fragment_view_positions_drives, container, false);
+        spinner=v.findViewById(R.id.drivesSpinner);
+        ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(getActivity(),
+                R.array.drives_spinner_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        return v;
     }
 
 }
